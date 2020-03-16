686242da440b550ba17e340aab974d6cde78f584
jcheron <myaddressmail@gmail.com>
2020-03-16 01:41:07 +0100

    [skip ci] opt prepared queries

--- src/Ubiquity/orm/core/prepared/DAOPreparedQuery.php
+++ src/Ubiquity/orm/core/prepared/DAOPreparedQuery.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Ubiquity\orm\core\prepared;
 
 use Ubiquity\db\SqlUtils;
@@ -16,6 +15,7 @@ use Ubiquity\orm\parser\ConditionParser;
  *
  */
 abstract class DAOPreparedQuery {
+
 	protected $databaseOffset;
 
 	/**
@@ -23,23 +23,39 @@ abstract class DAOPreparedQuery {
 	 * @var ConditionParser
 	 */
 	protected $conditionParser;
+
 	protected $included;
+
 	protected $hasIncluded;
+
 	protected $useCache;
+
 	protected $className;
+
 	protected $tableName;
+
 	protected $invertedJoinColumns = null;
+
 	protected $oneToManyFields = null;
+
 	protected $manyToManyFields = null;
+
 	protected $transformers;
+
 	protected $propsKeys;
+
 	protected $accessors;
+
 	protected $fieldList;
+
 	protected $firstPropKey;
+
 	protected $condition;
+
 	protected $preparedCondition;
-	protected $additionalMembers = [ ];
-	protected $sqlAdditionalMembers = "";
+
+	protected $loadObjectFromRowCallback;
+
 	/**
 	 *
 	 * @var \Ubiquity\db\Database
@@ -50,9 +66,18 @@ abstract class DAOPreparedQuery {
 		$this->className = $className;
 		$this->included = $included;
 		$this->condition = $condition;
-		$this->conditionParser = new ConditionParser ( $condition );
-		$this->prepare ();
-		$this->preparedCondition = SqlUtils::checkWhere ( $this->conditionParser->getCondition () );
+		$this->conditionParser = new ConditionParser($condition);
+		$this->prepare();
+		$this->preparedCondition = SqlUtils::checkWhere($this->conditionParser->getCondition());
+		if ($this->hasIncluded) {
+			$this->loadObjectFromRowCallback = function ($db, $row, $className, &$invertedJoinColumns, &$manyToOneQueries, &$oneToManyFields, &$manyToManyFields, &$oneToManyQueries, &$manyToManyParsers, &$accessors, &$transformers) {
+				return DAO::_loadObjectFromRow($db, $row, $className, $invertedJoinColumns, $manyToOneQueries, $oneToManyFields, $manyToManyFields, $oneToManyQueries, $manyToManyParsers, $accessors, $transformers);
+			};
+		} else {
+			$this->loadObjectFromRowCallback = function ($db, $row, $className, &$invertedJoinColumns, &$manyToOneQueries, &$oneToManyFields, &$manyToManyFields, &$oneToManyQueries, &$manyToManyParsers, &$accessors, &$transformers) {
+				return DAO::_simpleLoadObjectFromRow($db, $row, $className, $accessors, $transformers);
+			};
+		}
 	}
 
 	public function getFirstPropKey() {
@@ -168,62 +193,22 @@ abstract class DAOPreparedQuery {
 	}
 
 	protected function prepare() {
-		$this->db = DAO::getDb ( $this->className );
-		$this->included = DAO::_getIncludedForStep ( $this->included );
+		$this->db = DAO::getDb($this->className);
+		$this->included = DAO::_getIncludedForStep($this->included);
 
-		$metaDatas = OrmUtils::getModelMetadata ( $this->className );
-		$this->tableName = $metaDatas ['#tableName'];
-		$this->hasIncluded = $this->included || (\is_array ( $this->included ) && \sizeof ( $this->included ) > 0);
+		$metaDatas = OrmUtils::getModelMetadata($this->className);
+		$this->tableName = $metaDatas['#tableName'];
+		$this->hasIncluded = $this->included || (\is_array($this->included) && \sizeof($this->included) > 0);
 		if ($this->hasIncluded) {
-			DAO::_initRelationFields ( $this->included, $metaDatas, $this->invertedJoinColumns, $this->oneToManyFields, $this->manyToManyFields );
+			DAO::_initRelationFields($this->included, $metaDatas, $this->invertedJoinColumns, $this->oneToManyFields, $this->manyToManyFields);
 		}
-		$this->transformers = $metaDatas ['#transformers'] [DAO::$transformerOp] ?? [ ];
-		$this->fieldList = DAO::_getFieldList ( $this->tableName, $metaDatas );
-		$this->propsKeys = OrmUtils::getPropKeys ( $this->className );
-		$this->accessors = $metaDatas ['#accessors'];
-		$this->firstPropKey = OrmUtils::getFirstPropKey ( $this->className );
+		$this->transformers = $metaDatas['#transformers'][DAO::$transformerOp] ?? [];
+		$this->fieldList = DAO::_getFieldList($this->tableName, $metaDatas);
+		$this->propsKeys = OrmUtils::getPropKeys($this->className);
+		$this->accessors = $metaDatas['#accessors'];
+		$this->firstPropKey = OrmUtils::getFirstPropKey($this->className);
 	}
 
-	protected function updateSqlAdditionalMembers() {
-		if (\count ( $this->additionalMembers ) > 0) {
-			$this->sqlAdditionalMembers = ',' . $this->parseExpressions ();
-		}
-	}
-
-	protected function parseExpressions() {
-		return \implode ( ',', $this->additionalMembers );
-	}
-
-	protected function addAditionnalMembers($object, $row) {
-		foreach ( $this->additionalMembers as $member => $_ ) {
-			$object->{$member} = $row [$member] ?? null;
-			$object->_rest [$member] = $row [$member] ?? null;
-		}
-	}
-
-	abstract public function execute($params = [ ], $useCache = false);
-
-	/**
-	 * Adds a new expression and associates it with a new member of the class added at runtime.
-	 *
-	 * @param string $sqlExpression The SQL expression (part of the SQL SELECT)
-	 * @param string $memberName The new associated member name
-	 */
-	public function addMember(string $sqlExpression, string $memberName): void {
-		$this->additionalMembers [$memberName] = $sqlExpression . " AS '{$memberName}'";
-		$this->updateSqlAdditionalMembers ();
-	}
-
-	/**
-	 * Adds new expressions and their associated members at runtime.
-	 *
-	 * @param array $expressionsNames An associative array of [memberName=>sqlExpression,...]
-	 */
-	public function addMembers(array $expressionsNames): void {
-		foreach ( $expressionsNames as $member => $expression ) {
-			$this->additionalMembers [$member] = $expression . " AS '{$member}'";
-		}
-		$this->updateSqlAdditionalMembers ();
-	}
+	abstract public function execute($params = [], $useCache = false);
 }
 
--- src/Ubiquity/orm/core/prepared/DAOPreparedQueryById.php
+++ src/Ubiquity/orm/core/prepared/DAOPreparedQueryById.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Ubiquity\orm\core\prepared;
 
 use Ubiquity\events\DAOEvents;
@@ -12,40 +11,40 @@ use Ubiquity\orm\OrmUtils;
  * This class is part of Ubiquity
  *
  * @author jcheron <myaddressmail@gmail.com>
- * @version 1.0.1
+ * @version 1.0.2
  *
  */
 class DAOPreparedQueryById extends DAOPreparedQuery {
 
 	public function __construct($className, $included = false) {
-		parent::__construct ( $className, '', $included );
+		parent::__construct($className, '', $included);
 	}
 
 	protected function prepare() {
-		parent::prepare ();
-		$keys = OrmUtils::getKeyFields ( $this->className );
-		$this->conditionParser->addKeyValues ( \array_fill ( 0, \count ( $keys ), '' ), $this->className );
-		$this->conditionParser->limitOne ();
+		parent::prepare();
+		$keys = OrmUtils::getKeyFields($this->className);
+		$this->conditionParser->addKeyValues(\array_fill(0, \count($keys), ''), $this->className);
+		$this->conditionParser->limitOne();
 	}
 
-	public function execute($params = [ ], $useCache = false) {
+	public function execute($params = [], $useCache = false) {
 		$cp = $this->conditionParser;
-		$cp->setKeyValues ( $params );
-		$query = $this->db->prepareAndExecute ( $this->tableName, $this->preparedCondition, $this->fieldList . $this->sqlAdditionalMembers, $cp->getParams (), $useCache );
-		if ($query && \sizeof ( $query ) > 0) {
-			$oneToManyQueries = [ ];
-			$manyToOneQueries = [ ];
-			$manyToManyParsers = [ ];
+		$cp->setKeyValues($params);
+		$query = $this->db->prepareAndExecute($this->tableName, $this->preparedCondition, $this->fieldList, $cp->getParams(), $useCache);
+		if ($query && \sizeof($query) > 0) {
+			$call = $this->loadObjectFromRowCallback;
+			$oneToManyQueries = $manyToOneQueries = $manyToManyParsers = [];
 			$className = $this->className;
-			$row = \current ( $query );
-			$object = DAO::_loadObjectFromRow ( $this->db, $row, $className, $this->invertedJoinColumns, $manyToOneQueries, $this->oneToManyFields, $this->manyToManyFields, $oneToManyQueries, $manyToManyParsers, $this->accessors, $this->transformers );
-			$this->addAditionnalMembers ( $object, $row );
+			$object = $call($this->db, \current($query), $className, $this->invertedJoinColumns, $manyToOneQueries, $this->oneToManyFields, $this->manyToManyFields, $oneToManyQueries, $manyToManyParsers, $this->accessors, $this->transformers);
 			if ($this->hasIncluded) {
-				DAO::_affectsRelationObjects ( $className, $this->firstPropKey, $manyToOneQueries, $oneToManyQueries, $manyToManyParsers, [ $object ], $this->included, $useCache );
+				DAO::_affectsRelationObjects($className, $this->firstPropKey, $manyToOneQueries, $oneToManyQueries, $manyToManyParsers, [
+					$object
+				], $this->included, $useCache);
 			}
-			EventsManager::trigger ( DAOEvents::GET_ONE, $object, $className );
+			EventsManager::trigger(DAOEvents::GET_ONE, $object, $className);
 			return $object;
 		}
 		return null;
 	}
 }
+
